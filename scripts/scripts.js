1e4168792ed3008f9b4607be181031e2b37351c1
norraynor <37270508+Norraynor@users.noreply.github.com>
2023-10-03 21:06:45 +0200

    Add color change on mouse enter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,14 @@
 //create 16x16 square
 const size = 16;
+const BACKGROUNDCOLOR = "blue";
 
 const container = document.querySelector(".container");
 
 for (let i = 0; i < size*size; i++) {
     const div = document.createElement("div");
     div.classList.add("grid-element");
+    div.addEventListener('mouseenter', (e) => {
+        div.style.backgroundColor = BACKGROUNDCOLOR;
+    })
     container.appendChild(div);
 }
