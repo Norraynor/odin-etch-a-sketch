c3b1e400ded12f5d380a98f59a599719bc130386
norraynor <37270508+Norraynor@users.noreply.github.com>
2023-10-05 19:22:42 +0200

    Add function to remove all children from parent

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,7 +2,12 @@
 const size = 16;
 const BACKGROUNDCOLOR = "blue";
 
+const resizeButton = document.querySelector(".resize");
 const container = document.querySelector(".container");
+
+resizeButton.addEventListener('click', (e) => {
+    removeAllChildren(container);
+})
 
 for (let i = 0; i < size * size; i++) {
     const divSize = (1 / size)*100;
@@ -13,4 +18,12 @@ for (let i = 0; i < size * size; i++) {
         div.style.backgroundColor = BACKGROUNDCOLOR;
     })
     container.appendChild(div);
+}
+
+function removeAllChildren(parent) {
+    let child = parent.lastElementChild;
+    while (child) {
+        parent.removeChild(child);
+        child = parent.lastElementChild;
+    }
 }
