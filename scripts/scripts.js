a962a3d793987b860d428eb7d276eb089f6f2f9a
norraynor <37270508+Norraynor@users.noreply.github.com>
2023-10-05 19:42:35 +0200

    Add button for changing colors

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,38 @@
 //create 16x16 square
-const size = 16;
 const BACKGROUNDCOLOR = "blue";
+const MAX_SIZE = 50;
 
 const resizeButton = document.querySelector(".resize");
+const monocolorButton = document.querySelector(".monocolor");
 const container = document.querySelector(".container");
+let monoColor = true;
 
 resizeButton.addEventListener('click', (e) => {
-    removeAllChildren(container);
+    let newSize = +prompt(`How big should be new canva?(current max size is ${MAX_SIZE})`);
+    if (!isNaN(newSize)) {
+        removeAllChildren(container);   
+        if (newSize > MAX_SIZE) {
+            newSize = MAX_SIZE;
+        }
+        if (newSize < 0) {
+            newSize = 0;
+        }
+        createCanva(newSize);
+    }
+
 })
 
-for (let i = 0; i < size * size; i++) {
-    const divSize = (1 / size)*100;
-    const div = document.createElement("div");
-    div.classList.add("grid-element");
-    div.style.flexBasis = divSize + "%";
-    div.addEventListener('mouseenter', (e) => {
-        div.style.backgroundColor = BACKGROUNDCOLOR;
-    })
-    container.appendChild(div);
-}
+monocolorButton.addEventListener('click', (e) => {
+    if (monoColor) {
+        monocolorButton.textContent = "MONOCOLOR OFF";
+        monoColor = false;
+    } else {
+        monocolorButton.textContent = "MONOCOLOR ON";
+        monoColor = true;
+    }
+})
+
+createCanva();
 
 function removeAllChildren(parent) {
     let child = parent.lastElementChild;
@@ -26,4 +40,24 @@ function removeAllChildren(parent) {
         parent.removeChild(child);
         child = parent.lastElementChild;
     }
+}
+
+function createCanva(size=16) {
+    for (let i = 0; i < size * size; i++) {
+			const divSize = (1 / size) * 100;
+			const div = document.createElement("div");
+			div.classList.add("grid-element");
+			div.style.flexBasis = divSize + "%";
+        div.addEventListener("mouseenter", (e) => {
+            let selectedColor = "";
+            if (monoColor) {
+                selectedColor = BACKGROUNDCOLOR;
+            } else {
+                let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+                selectedColor = "#"+randomColor;
+            }
+            div.style.backgroundColor = selectedColor;
+			});
+			container.appendChild(div);
+		}
 }
