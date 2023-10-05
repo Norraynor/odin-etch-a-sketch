c7dab251b0e8adc09d34d6046374ef2618109074
norraynor <37270508+Norraynor@users.noreply.github.com>
2023-10-05 19:14:36 +0200

    Add variable to change canva size

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,9 +4,11 @@ const BACKGROUNDCOLOR = "blue";
 
 const container = document.querySelector(".container");
 
-for (let i = 0; i < size*size; i++) {
+for (let i = 0; i < size * size; i++) {
+    const divSize = (1 / size)*100;
     const div = document.createElement("div");
     div.classList.add("grid-element");
+    div.style.flexBasis = divSize + "%";
     div.addEventListener('mouseenter', (e) => {
         div.style.backgroundColor = BACKGROUNDCOLOR;
     })
